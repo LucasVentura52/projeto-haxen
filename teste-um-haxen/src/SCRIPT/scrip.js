93005f57c8359d4b06eeba4bf060e0060074c571
Lucas Ventura <venturalucascruz@gmail.com>
2024-10-05 22:42:34 -0300

    CÓDIGO MAIS RECENTE
    CÓDIGO MAIS RECENTE

--- teste-um-haxen/src/SCRIPT/scrip.js
+++ teste-um-haxen/src/SCRIPT/scrip.js
@@ -12,6 +12,15 @@ document.getElementById('openModal').addEventListener('click', function () {
         .catch(error => console.error('Erro ao carregar a nova página:', error));
 });
 
+function teste(){
+    fetch('src/HTML/page.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('modalBody').innerHTML = data;
+        $('#infoModal1').modal('show');
+    })
+}
+
 document.getElementById('openModal2').addEventListener('click', function () {
     fetch('page.html')
         .then(response => response.text())
@@ -25,8 +34,36 @@ document.getElementById('openModal2').addEventListener('click', function () {
 
 document.getElementById("year").textContent = new Date().getFullYear();
 
-function teste(){
-    $(document).ready(function () {
-        $('#infoModal').modal('show');
-    });
+
+
+
+
+
+
+//SCRIP BUTTON CARD
+function abrirModalCard1() {
+    fetch('src/HTML/card-pages/pageCard1.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('modalBody1').innerHTML = data;
+        $('#infoModalCard1').modal('show');
+    })
+}
+
+function abrirModalCard2() {
+    fetch('src/HTML/card-pages/pageCard2.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('modalBody2').innerHTML = data;
+        $('#infoModalCard2').modal('show');
+    })
+}
+
+function abrirModalCard3() {
+    fetch('src/HTML/card-pages/pageCard3.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('modalBody3').innerHTML = data;
+        $('#infoModalCard3').modal('show');
+    })
 }
